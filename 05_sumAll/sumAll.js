6110e03d9794d8bb5ccf3d320924858ef6fd0730
AlanJGG <alanguzmangutierrez2005@gmail.com>
2024-04-22 18:45:50 -0600

    Finish 01 to 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(A, B) {
+    
+    if(!Number.isInteger(A) || !Number.isInteger(B)) return "ERROR";
+    if(A < 0 || B < 0) return "ERROR";
 
+    let sum = 0;
+
+    if(A > B)
+        [A, B] = [B, A];
+        
+    for(let i=A; i<=B; i++)
+        sum += i;
+    
+    return sum;
 };
 
 // Do not edit below this line
